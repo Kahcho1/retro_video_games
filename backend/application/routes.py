a2a1fdd2a351b16a91b2cf3301d9c5d10d5dbb69
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-30 10:21:27 +0000

    changes

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -1,65 +1,53 @@
 from application import app, db
-from application.models import Tasks
+from application.models import Games, Console
 from flask import request, redirect, url_for, Response, jsonify
 
-@app.route('/create/task', methods=['POST'])
-def create_task():
+@app.route('/create/game', methods=['POST'])
+def create_game():
     package = request.json
-    task_new = Tasks(desc=package["description"])
-    db.session.add(task_new)
+    game_new = Games(desc=package["description"])
+    db.session.add(game_new)
     db.session.commit()
-    return Response(f"Task with description has been added: {task_new.desc}", mimetype='text/plain')
+    return Response(f"Game with description has been added: {game_new.description}", mimetype='text/plain')
 
-@app.route('/read/allTasks', methods=['GET'])
-def read_tasks():
-    all_tasks = Tasks.query.all()
-    t_dict = {"tasks": []}
+@app.route('/read/allGames', methods=['GET'])
+def read_games():
+    all_games = Games.query.all()
+    g_dict = {"games": []}
 
-    for task in all_tasks:
-        t_dict["tasks"].append(
+    for games in all_games:
+        g_dict["games"].append(
             {
-                "id": task.id,
-                "description": task.desc,
-                "completed": task.comp
+                "id": games.id,
+                "Name": games.gname,
+                "description": games.description,
+                "Release Date": games.gdate
             }
         )
-    return jsonify(t_dict)
+    return jsonify(g_dict)
 
-@app.route('/read/tasks/<int:id>', methods=['GET'])
-def read_task(id):
-    task = Tasks.query.get(id)
-    t_dict = {
-                "id": task.id,
-                "description": task.desc,
-                "completed": task.comp
+@app.route('/read/games/<int:id>', methods=['GET'])
+def read_game(id):
+    game = Tasks.query.get(id)
+    g_dict = {
+                "id": games.id,
+                "Name": games.gname,
+                "description": games.description,
+                "Release Date": games.gdate
             }
-    return jsonify(t_dict)
+    return jsonify(g_dict)
 
-@app.route('/update/task/<int:id>', methods=['PUT'])
-def update_task(id):
-    package = request.json
-    task = Tasks.query.get(id)
-    task.desc = package["description"]
-    db.session.commit()
-    return Response(f"Task #{id} has been updated with description: {task.desc}", mimetype='text/plain')
+# @app.route('/update/task/<int:id>', methods=['PUT'])
+# def update_task(id):
+#     package = request.json
+#     task = Tasks.query.get(id)
+#     task.desc = package["description"]
+#     db.session.commit()
+#     return Response(f"Task #{id} has been updated with description: {task.desc}", mimetype='text/plain')
 
-@app.route('/delete/task/<int:id>', methods=['DELETE'])
-def delete(id):
-    task = Tasks.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return Response(f"Task #{id} has been deleted!", mimetype='text/plain')
-
-@app.route('/complete/task/<int:id>', methods=['PUT'])
-def status(id):
-    task = Tasks.query.get(id)
-    task.comp = True
-    db.session.commit()
-    return Response(f"Task #{id} status has been changed to completed.", mimetype='text/plain')
-
-@app.route('/incomplete/task/<int:id>', methods=['PUT'])
-def status_incomp(id):
-    task = Tasks.query.get(id)
-    task.comp = False
-    db.session.commit()
-    return Response(f"Task #{id} status has been changed to incompleted.", mimetype='text/plain')
+# @app.route('/delete/task/<int:id>', methods=['DELETE'])
+# def delete(id):
+#     task = Tasks.query.get(id)
+#     db.session.delete(task)
+#     db.session.commit()
+#     return Response(f"Task #{id} has been deleted!", mimetype='text/plain')
