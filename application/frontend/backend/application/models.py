424af03a445ed42361f83d60b990792fc59f2d50
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-30 09:22:56 +0000

    init fix

--- application/frontend/backend/application/models.py
+++
@@ -1,6 +0,0 @@
-from application import db
-
-class Tasks(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    desc = db.Column(db.String(30), nullable=True)
-    comp = db.Column(db.Boolean, nullable=False, default=False)
