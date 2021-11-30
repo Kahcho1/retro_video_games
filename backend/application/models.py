2a5cb55f3453754233996ebe60797e62c2ff665a
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-11-30 13:30:15 +0000

    forms

--- backend/application/models.py
+++ backend/application/models.py
@@ -5,10 +5,10 @@ class Games(db.Model):
     game_name = db.Column(db.String(30), nullable=True)
     description = db.Column(db.String(200), nullable=True)
     date = db.Column(db.DateTime(), nullable=True)
-    console = db.relationship("Console", backref="games")
+    games = db.Column(db.Integer, db.ForeignKey('console.id'), nullable=False)
 
 class Console(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     console_name = db.Column(db.String(30), nullable=True)
     date = db.Column(db.DateTime(), nullable=True)
-    games_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    console = db.relationship("Games", backref="console")
--- frontend/application/forms.py
+++ frontend/application/forms.py
@@ -4,8 +4,11 @@ from wtforms.validators import DataRequired
 
 class GamesForm(FlaskForm):
     name = StringField("Add a name of a game: ", validators=[DataRequired()])
+    description = StringField("Add a summary for the game: ")
+    date = DateTimeField("Add the game's date of release: ", validators=[DataRequired()])
     submit = SubmitField("Enter")
 
 class ConsoleForm(FlaskForm):
     name = StringField("Add a platform: ", validators=[DataRequired()])
+    date = DateTimeField("Add the console's date of release: ", validators=[DataRequired()])
     submit = SubmitField("Enter")
