601bd8c35e46afec6ca5ba3f35b8315b6ed95b80
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-12-03 18:57:22 +0000

    fixes

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -13,7 +13,6 @@ test_game = {
             }
 
 test_console = {
-
                 "id": 1,
                 "name": "N64",
                 "release_date": datetime(1996,9,20)
@@ -42,47 +41,40 @@ class TestRead(TestBase):
 
     def test_read_vgdb(self):
         response = self.client.get(url_for('read_game'))
-        all_games = { "games": [test_game]}
+        all_games = { "games": [test_game] }
         self.assertEquals(all_games, response.json)
     
     def test_read_cdb(self):
         response = self.client.get(url_for('read_console'))
-        all_console = { "console": [test_console]}
+        all_console = { "console": [test_console] }
         self.assertEquals(all_console, response.json)
 
     def test_read_one_game(self):
         response = self.client.get(url_for("read_game_one", id=1))
+        all_games = { "games": [test_game] }
         self.assertEquals(all_games, response.json)
     
     def test_read_one_console(self):
         response = self.client.get(url_for("read_console_one", id=1))
+        all_console = { "console": [test_console] }
         self.assertEquals(all_console, response.json)
 
 class TestCreate(TestBase):
     def test_add_game(self):
         response = self.client.post(
             url_for('add_game'),
-            json={
-                "name": "Final Fantasy VII",
-                "description": "lets mosey", 
-                "release_date": datetime(1997,7,17), 
-                "console": "PSZ"
-                },
+            json={"name": "Final Fantasy VII", "description": "lets mosey", "release_date": datetime(1997,7,17), "console": "PSZ"},
             follow_redirects=True
-            )
+        )
         self.assertEquals(b"Add test game entry", response.data)
         self.assertEquals(Games.query.get(2).game_name, "Testing add games")
 
     def test_add_platform(self):
         response = self.client.post(
             url_for('add_platform'),
-            json={
-                "id": 2,
-                "name": "Test Console",
-                "release_date": datetime(1995,5,15)
-                },
+            json={"id": 2, "name": "Test Console", "release_date": str(datetime(1995,5,15))},
             follow_redirects=True
-            )
+        )
         self.assertEquals(b"Add test console entry", response.data)
         self.assertEquals(Console.query.get(2).console_name, "Testing add console")
 
@@ -92,16 +84,16 @@ class TestUpdate(TestBase):
             url_for('update_game', id=1),
             json={"name": "Testing update game"},
         )
-        self.assertEquals(b"Game name with ID 1 has been changed to: Testing update game", response.data)
-        self.assertEqual(Console.query.get(1).name, "Testing update game")
+        self.assertEquals(b"Game name has been changed to: Testing update game", response.data)
+        self.assertEquals(Games.query.get(1).name, "Testing update game")
 
     def test_update_platform(self):
         response = self.client.put(
             url_for('update_platform', id=1),
-            json={"name": "Testing update console"},
+            json={"name": "Testing update console"}
         )
-        self.assertEquals(b"Console name with ID 1 has been changed to: Testing update console", response.data)
-        self.assertEqual(Console.query.get(1).name, "Testing update console")
+        self.assertEquals(b"Console name has been changed to: Testing update console", response.data)
+        self.assertEquals(Console.query.get(1).name, "Testing update console")
 
 
 class TestDelete(TestBase):
@@ -112,5 +104,5 @@ class TestDelete(TestBase):
 
     def test_delete_platform(self):
         response = self.client.delete(url_for("delete_platform", id=1))
-        self.assertEquals(b"Delete console with ID 1", response.data)
+        self.assertEquals(b"Testing update console has been deleted!", response.data)
         self.assertIsNone(Console.query.get(1))
