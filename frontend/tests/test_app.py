00ee12b21e074a1b1a965955a5e1d94d6e79348e
Ubuntu <kaho@dev-jenkins.x5jitdqegk3uxjg0e5j5gsbjld.cwx.internal.cloudapp.net>
2021-12-03 09:39:21 +0000

    init Jenkins

--- frontend/tests/test_app.py
+++ frontend/tests/test_app.py
@@ -6,7 +6,7 @@ import requests_mock
 
 test_game = {
                 "id": 1,
-                "names": "whatever",
+                "name": "whatever",
                 "release_date": 11/1/1991,
                 "console": "PST",
                 "description": "Test Test Test"
@@ -14,7 +14,7 @@ test_game = {
 
 test_console = {
                     "id": 1,
-                    "names": "Test console",
+                    "name": "Test console",
                     "release_date": 22/2/1992
                 }
 
@@ -44,10 +44,39 @@ class TestViews(TestBase):
             self.assert200(response)
 
     def test_add_get(self):
-        response = self.client.get(url_for('add_game'), ('read_cdb'))
+        response = self.client.get(url_for('add_game'))
         self.assert200(response)
 
     def test_add_console_get(self):
         response = self.client.get(url_for('add_platform'))
         self.assert200(response)
 
+
+
+
+class TestRead(TestBase):
+
+    def test_read_home_game(self):
+        with requests_mock.Mocker() as m:
+            all_games = {"games": [test_game]}
+            m.get(f"http://{backend_host}/read/vgdb", json=all_games)
+            response = self.client.get(url_for('home'))
+            self.assertIn(b"Test Test Test", response.data)
+
+    def test_read_home_console(self):
+        with requests_mock.Mocker() as m:
+            all_console = {"console": [test_console]}
+            m.get(f"http://{backend_host}/read/cdb", json=all_console)
+            response = self.client.get(url_for('home_console'))
+            self.assertIn(b"Test console", response.data)
+
+class TestCreate(TestBase):
+    def test_add_game(self):
+        with requests_mock.Mocker() as m:
+        m.post(f"http://{backend_host}/add/game", data="Testing add")
+        response = self.client.post(
+            url_for('add_game'),
+            data={"name": "Testing Game Name"},
+            follow_redirects=True
+            )
+        self.assertIn(b"Testing Game Name", response.data)
